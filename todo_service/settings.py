da06a6187a5ad536526842d4ce0d2c915546fb6e
4ikkamony <compbanderloga@gmail.com>
2025-01-31 11:00:23 +0200

    Install django_crispy_forms and crispy_bootstrap5

--- todo_service/settings.py
+++ todo_service/settings.py
@@ -37,7 +37,14 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+
+    # crispy
+    "crispy_forms",
+    "crispy_bootstrap5",
+
+    # my app
     "todo",
+
 ]
 
 MIDDLEWARE = [
@@ -67,6 +74,11 @@ TEMPLATES = [
         },
     },
 ]
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+
+CRISPY_TEMPLATE_PACK = "bootstrap5"
+
 
 WSGI_APPLICATION = "todo_service.wsgi.application"
 
