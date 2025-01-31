4b054b5df8fa8f6f36d84515706f5af626dbfc94
4ikkamony <compbanderloga@gmail.com>
2025-01-31 15:49:01 +0200

    Add list views for Task and Tag, as well as Create, Update, Delete views for each
    Update todo/urls.py accordingly

--- todo/views.py
+++ todo/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.urls import reverse, reverse_lazy
 from django.views import generic
+
 from todo.models import Task, Tag
+from todo.forms import TaskForm, TagForm
 
 
 class TaskListView(generic.ListView):
@@ -10,8 +13,49 @@ class TaskListView(generic.ListView):
     paginate_by = 5
 
 
+class TaskCreateView(generic.CreateView):
+    model = Task
+    form_class = TaskForm
+    success_url = reverse_lazy("todo:task-list")
+
+
+class TaskUpdateView(generic.UpdateView):
+    model = Task
+    form_class = TaskForm
+    success_url = reverse_lazy("todo:task-list")
+
+class TaskDeleteView(generic.DeleteView):
+    model = Task
+    success_url = reverse_lazy("todo:task-list")
+
+
 class TagListView(generic.ListView):
     model = Tag
     template_name = "todo/tag_list.html"
-    paginate_by = 10
+    paginate_by = 9
     queryset = Tag.objects.prefetch_related("tasks")
+
+
+class TagCreateView(generic.CreateView):
+    model = Tag
+    form_class = TagForm
+    success_url = reverse_lazy("todo:tag-list")
+
+
+class TagUpdateView(generic.UpdateView):
+    model = Tag
+    form_class = TagForm
+    success_url = reverse_lazy("todo:tag-list")
+
+
+class TagDeleteView(generic.DeleteView):
+    models = Tag
+    success_url = reverse_lazy("todo:tag-list")
+
+
+class ToggleTaskStatus(generic.View):
+    def post(self, request, pk, *args, **kwargs):
+        task = get_object_or_404(Task, pk=pk)
+        task.is_done = not task.is_done
+        task.save()
+        return redirect(reverse("todo:task-list"))
